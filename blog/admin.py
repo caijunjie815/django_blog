6a361ea95f09d8962c1dd79e91a069bec72df5da
eddy <cjj1992815@gmail.com>
2018-07-27 07:17:04 +1000

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'author', 'category', 'posted_time')
+	list_filter = ['posted_time', 'category']
+	search_fields = ['title', 'content']
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+	list_display = ("article", 'name', 'posted_time', 'content', 'reply')
+	list_filter = ['article']
+
+
+admin.site.register((Category, Tag))
---
+++ blog/models.py
@@ -0,0 +1,60 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.shortcuts import reverse
+
+
+# Create your models here.
+class Category(models.Model):
+	name = models.CharField(max_length=100, unique=True, help_text='Enter a post category (e.g. python)')
+
+	def get_absolute_url(self):
+		"""Returns the url to access a particular instance of the model."""
+		return reverse('blog:category', args=[str(self.id)])
+
+	def __str__(self):
+		return self.name
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=20, unique=True)
+
+	def __str__(self):
+		return self.name
+
+	def get_absolute_url(self):
+		pass
+
+
+class Post(models.Model):
+	author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+	title = models.CharField(max_length=100, unique=True)
+	content = models.TextField(help_text='Enter your post content here')
+	posted_time = models.DateField('date posted', auto_now=True)
+	category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=1)
+	tag = models.ManyToManyField(Tag)
+
+	class Meta:
+		ordering = ['-id']
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		"""Returns the url to access a particular instance of the model."""
+		return reverse('blog:article', args=[str(self.id)])
+
+
+class Comments(models.Model):
+	name = models.CharField(max_length=20)
+	email = models.EmailField()
+	content = models.TextField()
+	posted_time = models.DateTimeField(auto_now=True)
+	article = models.ForeignKey(Post, on_delete=models.CASCADE)
+	reply = models.ForeignKey('self', on_delete=models.DO_NOTHING, null=True, blank=True)
+
+	def __str__(self):
+		return self.content
+
+	def get_absolute_url(self):
+		"""Returns the url to access a particular instance of the model."""
+		pass
